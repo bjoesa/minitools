cffa5b9129e4fa6f47d3e8fbf66ba0ebedcf8484
bjoesa <bjoern.sass@fau.de>
2019-11-15 11:53:54 +0100

    change 15:11.

--- link_checker.py
+++ link_checker.py
@@ -4,14 +4,14 @@ import certifi
 
 http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
 url = input('Type your URL: ')
-website = http.urlopen('GET', url)
+website = http.urlopen('GET', url)  # try if url startswidth: 'http' else: raise exception
 soup = BeautifulSoup(website.data, "lxml")
 
 print('-------------------')
 sitestatus = website.status
 if sitestatus == 200:
     print('Website status OK (', sitestatus,')')
-elif sitestatus == 401:
+elif sitestatus > 399:
     print('Website status: ', sitestatus,' (unauthorized)')
 else:
     print('Website status: ', sitestatus,' (sth. is strange `O.o´)')
@@ -26,13 +26,12 @@ print('-------------------')
 print('Link list and connection test: ')
 
 for link in soup.findAll('a'):
-    print(link.get('href'))
+    print('----------\nlink address:', link.get('href'))
     try:
         r = http.request('GET', link.get('href'))
         print('status: ', r.status)
     except:
         # if Exception is raised
         print('no connection test...')
-        pass
 
 print('\n---- FINISH ----')
